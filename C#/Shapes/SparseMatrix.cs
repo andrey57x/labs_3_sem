64662016827ed76d7f15092a0ab416d70ab1bbe6
Saf1k <57andreys57@gmail.com>
2024-09-29 20:01:49 +0300

    Labs C# 2-4

---
+++ C#/Shapes/SparseMatrix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    internal class SparseMatrix<T>
+    {
+        private Dictionary<int[], T> matrix;
+        public SparseMatrix()
+        {
+            matrix = [];
+        }
+        public T this[int k1, int k2, int k3]
+        {
+            get
+            {
+                foreach (int[] i in matrix.Keys)
+                {
+                    if (i[0] == k1 && i[1] == k2 && i[2] == k3)
+                    {
+                        return matrix[i];
+                    }
+                }
+                throw new KeyNotFoundException("Нет такого");
+            }
+            set
+            {
+                matrix[[k1, k2, k3]] = value;
+            }
+        }
+    }
+}
